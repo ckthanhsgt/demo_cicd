215ad2e636b3732cdd4cb1e37ef14e4e9eafc91b
ckthanh.sgt <ckthanh@saigon-tech.vn>
2025-01-02 13:45:59 +0700

    Initial Node.js app setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 82;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
